ee146bd67f5c534fbfceec4bfa450d996cbdb91d
Tri <trimpham98@gmail.com>
2024-05-18 23:25:10 -0700

    Add imports

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
-import './Navbar.css';
+import images from "../../constants/images";
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+import "./Navbar.css";
+
+const Navbar = () => <div>Navbar</div>;
 
 export default Navbar;
