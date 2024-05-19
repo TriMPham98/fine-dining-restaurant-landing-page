7a60a92b97757c0d45d8f9cb99eefb20aec907d2
Tri <trimpham98@gmail.com>
2024-05-19 00:14:44 -0700

    Add flexbox and padding for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,11 @@ const Navbar = () =>
     <img src={images.gericht} alt="app logo"/>
   </div>
   <ul className="app__navbar-links">
-    <li className="p__opensans"></li>
+    <li className="p__opensans"><a href="#home">Home</a></li>
+    <li className="p__opensans"><a href="#about">About</a></li>
+    <li className="p__opensans"><a href="#menu">Menu</a></li>
+    <li className="p__opensans"><a href="#awards">Awards</a></li>
+    <li className="p__opensans"><a href="#contact">Contact</a></li>
   </ul>
 </nav>;
 
