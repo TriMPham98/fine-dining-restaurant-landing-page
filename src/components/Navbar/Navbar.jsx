b596c829c2a86d3b35d32375700e7cdb6cd8acc9
Tri <trimpham98@gmail.com>
2024-05-21 10:55:17 -0700

    Add log in and book table button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,18 +6,35 @@ import images from "../../constants/images";
 
 import "./Navbar.css";
 
-const Navbar = () => 
-<nav className="app__navbar">
-  <div className="app__navbar-logo">
-    <img src={images.gericht} alt="app logo"/>
-  </div>
-  <ul className="app__navbar-links">
-    <li className="p__opensans"><a href="#home">Home</a></li>
-    <li className="p__opensans"><a href="#about">About</a></li>
-    <li className="p__opensans"><a href="#menu">Menu</a></li>
-    <li className="p__opensans"><a href="#awards">Awards</a></li>
-    <li className="p__opensans"><a href="#contact">Contact</a></li>
-  </ul>
-</nav>;
+const Navbar = () => (
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+      <img src={images.gericht} alt="app logo" />
+    </div>
+    <ul className="app__navbar-links">
+      <li className="p__opensans">
+        <a href="#home">Home</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#about">About</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#menu">Menu</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#awards">Awards</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#contact">Contact</a>
+      </li>
+    </ul>
+    <div className="app__navbar-login">
+      <a href="#login" className="p__opensans">
+        Log In / Register
+      </a>
+      <a href="/" className="p__opensans">Book Table</a>
+    </div>
+  </nav>
+);
 
 export default Navbar;
