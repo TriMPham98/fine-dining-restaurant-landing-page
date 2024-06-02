4d4a8794fcc6a79cb9ed1a430febb4d4361e65aa
Tri <trimpham98@gmail.com>
2024-06-02 10:39:40 -0700

    Format the file

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import './Footer.css';
+import "./Footer.css";
 
-const Footer = () => (
-  <div>
-    Footer
-  </div>
-);
+const Footer = () => <div>Footer</div>;
 
 export default Footer;
