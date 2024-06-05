2c7de20731a875fff4c1f270a655c92cabc9a501
Tri <trimpham98@gmail.com>
2024-06-05 12:45:04 -0700

    Format the file

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import './Gallery.css';
+import "./Gallery.css";
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => <div>Gallery</div>;
 
 export default Gallery;
