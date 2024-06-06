eb7df680baf4e582822de3280d7a68cc9dcc8b30
Tri <trimpham98@gmail.com>
2024-06-05 22:09:03 -0700

    Import footer overlay and images

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 
+import { FooterOverlay, Newsletter } from "../../components";
+import { images } from "../../constants";
 import "./Footer.css";
 
 const Footer = () => <div>Footer</div>;
