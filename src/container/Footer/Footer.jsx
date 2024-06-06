7fd6c12f079898f1b4294713984359c930afd787
Tri <trimpham98@gmail.com>
2024-06-05 22:09:46 -0700

    Change year of copyright to 2024

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -46,7 +46,7 @@ const Footer = () => (
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
+      <p className="p__opensans">2024 Gericht. All Rights reserved.</p>
     </div>
   </div>
 );
