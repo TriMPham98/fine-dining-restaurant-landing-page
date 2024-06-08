22cb13536e4dbbba338877b7668c5e4868fb0e9e
Tri <trimpham98@gmail.com>
2024-06-07 23:21:17 -0700

    Comment out footer overlay and newsletter import

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 
-import { FooterOverlay, Newsletter } from "../../components";
+// import { FooterOverlay, Newsletter } from "../../components";
 import { images } from "../../constants";
 import "./Footer.css";
 
