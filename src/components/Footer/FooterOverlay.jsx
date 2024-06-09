5e25003934915026b485efaa3dcca780bcccd4c9
Tri <trimpham98@gmail.com>
2024-06-08 23:51:20 -0700

    Add images to footer overlay

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -2,6 +2,11 @@ import React from "react";
 
 import "./FooterOverlay.css";
 
-const FooterOverlay = () => <div>FooterOverlay</div>;
+const FooterOverlay = () => (
+  <div className="app__footerOverlay">
+    <div className="app__footerOverlay-black" />
+    <div className="app__footerOverlay-img app__bg" />
+  </div>
+);
 
 export default FooterOverlay;
